f8a0939d01b591a11ef226890cf760b7d78b7721
sunghyun-park <love1092001@gmail.com>
2017-07-28 16:00:41 +0900

    ADD join
    join.jsp 추가
    MemberController.java에 join.do 기능 추가

--- MuchMore/src/main/java/com/spring/muchmore/member/MemberController.java
+++ MuchMore/src/main/java/com/spring/muchmore/member/MemberController.java
@@ -1,5 +1,14 @@
 package com.spring.muchmore.member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
+	
+	@RequestMapping("join.do")
+	public String join(){
+		return "join";
+	}
 
 }
