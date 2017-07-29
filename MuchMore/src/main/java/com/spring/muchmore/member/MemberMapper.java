e55d2b7cbe0a9f60ef5c9e24b5da493624bab4ec
healim0216 <healim0216@naver.com>
2017-07-29 16:38:00 +0900

    ADD FindAction in Login page
    MemberController : loginFindId.do / loginFindPw.do
    MemberMapper / MemberDAO / MemberDAOService : findId / findPw

--- MuchMore/src/main/java/com/spring/muchmore/member/MemberMapper.java
+++ MuchMore/src/main/java/com/spring/muchmore/member/MemberMapper.java
@@ -5,5 +5,11 @@ public interface MemberMapper {
 	/*2017-07-29 혜림 : 회원 정보 전체 가져오기.*/
 	public MemberVO getMember(MemberVO member);
 
+	/*2017-07-29 혜림 : 아이디 찾기 : 이름과 이메일을 입력하면 아이디 return*/
+	public String findId(MemberVO member);
+	
+	/*2017-07-29 혜림 : 비밀번호 찾기 : 아이디, 이름, 이메일을 입력하면 비밀번호 return*/
+	public String findPw(MemberVO member);
+	
 }
  
